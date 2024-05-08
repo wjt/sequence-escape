8f3ce9ce2b01b3e1c376627dcf2d26d24910eedd
Will Thompson <wjt@endlessos.org>
2024-05-08 11:01:19 +0100

    Move player based on input
    and add player to scene

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
+var speed = 300
+
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir.normalized() * speed
+
 func _process(_delta):
 	if Input.is_action_pressed("move_right"):
 		_animated_sprite.play("walk_right")
@@ -13,3 +19,7 @@ func _process(_delta):
 		_animated_sprite.play("walk_down")
 	else:
 		_animated_sprite.stop()
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
