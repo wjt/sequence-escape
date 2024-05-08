b099e42a09852bbd811611df1d1bdcbf414554c4
Will Thompson <wjt@endlessos.org>
2024-05-08 16:22:06 +0100

    switch: Animate switching on and off

--- scenes/switch.gd
+++ scenes/switch.gd
@@ -3,15 +3,14 @@ extends StaticBody2D
 @export_range(0, 2) var colour: int = 0:
 	set(new_colour):
 		colour = new_colour
-		update_sprite()
+		$Sprite2D.frame_coords.y = new_colour
 
 @export var switched_on: bool = false:
 	set(new_value):
+		if switched_on == new_value:
+			pass
+		elif new_value:
+			$AnimationPlayer.play("switch_on")
+		else:
+			$AnimationPlayer.play_backwards("switch_on")
 		switched_on = new_value
-		update_sprite()
-
-func _ready():
-	update_sprite()
-
-func update_sprite():
-	$Sprite2D.frame_coords = Vector2(2 if switched_on else 0, colour)
