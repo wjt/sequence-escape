d7a9ffd62949f0d2dbd471d05975ae27d86c4949
Will Thompson <wjt@endlessos.org>
2024-05-08 13:42:29 +0100

    Snap player to grid

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,24 +2,29 @@ extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
-var speed = 300
+var tile_size = 16  # FIXME
 
-func get_input():
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_dir.normalized() * speed
+var inputs = {
+	"move_right": Vector2.RIGHT,
+	"move_left": Vector2.LEFT,
+	"move_up": Vector2.UP,
+	"move_down": Vector2.DOWN,
+}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
 
 func _process(_delta):
-	if Input.is_action_pressed("move_right"):
-		_animated_sprite.play("walk_right")
-	elif Input.is_action_pressed("move_left"):
-		_animated_sprite.play("walk_left")
-	elif Input.is_action_pressed("move_up"):
-		_animated_sprite.play("walk_up")
-	elif Input.is_action_pressed("move_down"):
-		_animated_sprite.play("walk_down")
-	else:
-		_animated_sprite.stop()
+	for dir in inputs:
+		if Input.is_action_just_pressed(dir):
+			# FIXME: make animation names match actions
+			_animated_sprite.play(dir.replace("move_", "walk_"))
+			var tween = self.create_tween()
+			tween.set_trans(Tween.TRANS_LINEAR)
+			tween.tween_property(self, "position", position + inputs[dir] * tile_size, 0.5)
+			break
 
-func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+#func _physics_process(delta):
+	#get_input()
+	#move_and_collide(velocity * delta)
